29c62e03857e349e851568542c193caa6fa7e318
Akshala Bhatnagar <47988358+akshala@users.noreply.github.com>
2020-02-09 13:24:09 +0530

    question1

--- q1.py
+++ q1.py
@@ -27,30 +27,39 @@ print("DNA sequence: ", dna)
 rna = dna.replace("T", "U")
 print("corresponding RNA sequence: ", rna)
 
-dna_to_protein = {
-	'TTT':'F', 'TTC':'F', 'TTA':'L','TTG':'L',
-	'CTT':'L', 'CTC':'L', 'CTA':'L', 'CTG':'L',
-	'ATT':'I', 'ATC':'I', 'ATA':'I', 'ATG':'M',
-	'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',
-	'TCT':'S', 'TCC':'S', 'TCA':'S', 'TCG':'S',
-	'CCT':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
-	'ACT':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T',
-	'GCT':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
-	'TAT':'Y', 'TAC':'Y', 'TAA':'-', 'TAG':'-', 
-	'CAT':'H', 'CAC':'H', 'CAA':'Q', 'CAG':'Q',
-	'AAT':'N', 'AAC':'N', 'AAA':'K', 'AAG':'K', 
-	'GAT':'D', 'GAC':'D', 'GAA':'E', 'GAG':'A',
-	'TGT':'C', 'TGC':'C', 'TGA':'-', 'TGG':'W',
-	'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R',
-	'AGT':'S', 'AGC':'S', 'AGA':'R', 'AGG':'R',
-	'GGT':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G'
+rna_to_protein = {
+	'UUU':'F', 'UUC':'F', 'UUA':'L','UUG':'L',
+	'CUU':'L', 'CUC':'L', 'CUA':'L', 'CUG':'L',
+	'AUU':'I', 'AUC':'I', 'AUA':'I', 'AUG':'M',
+	'GUU':'V', 'GUC':'V', 'GUA':'V', 'GUG':'V',
+	'UCU':'S', 'UCC':'S', 'UCA':'S', 'UCG':'S',
+	'CCU':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
+	'ACU':'U', 'ACC':'U', 'ACA':'U', 'ACG':'U',
+	'GCU':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
+	'UAU':'Y', 'UAC':'Y', 'UAA':'-', 'UAG':'-', 
+	'CAU':'H', 'CAC':'H', 'CAA':'Q', 'CAG':'Q',
+	'AAU':'N', 'AAC':'N', 'AAA':'K', 'AAG':'K', 
+	'GAU':'D', 'GAC':'D', 'GAA':'E', 'GAG':'A',
+	'UGU':'C', 'UGC':'C', 'UGA':'-', 'UGG':'W',
+	'CGU':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R',
+	'AGU':'S', 'AGC':'S', 'AGA':'R', 'AGG':'R',
+	'GGU':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G'
 	}
 
 protein = ''
+flag = 0
 for i in range(0,length,3):
-	if(dna[i:i+3] == ''):
+	if(rna[i:i+3] == ''):
 		break
-	protein += dna_to_protein[dna[i:i+3]]
+	# print('codon: ', rna[i:i+3])
+	if(rna[i:i+3] == 'AUG'):
+		flag = 1;
+		# print("start codon")
+	if(rna[i:i+3] == 'UAA' or rna[i:i+3] == 'UAG' or rna[i:i+3] == 'UGA'):
+		# print("stop codon")
+		flag = 0
+	if flag:
+		protein += rna_to_protein[rna[i:i+3]]	
 print("corresponding Protein sequence: ", protein)
 
 file = open(output_file, 'w')
